a92f6271d3971ab0a3289ad2a47210fb6f075cdf
Paul Schoen <pschoen325@gmail.com>
2016-11-06 17:54:09 -0500

    added some jquery on projects still needs to be messed with though

--- bootflat/js/custom_js.js
+++ bootflat/js/custom_js.js
@@ -32,3 +32,22 @@ $(function() {
     }
   });
 });
+
+$(".wynork").hover(function () {
+      $(".stage").css({ "background": "url('https://i.gyazo.com/ec05fe1c0476a089186f03c44889fff7.gif')" });
+   },
+   function () {
+      $(".stage").css({ "background": "https://i.gyazo.com/ec05fe1c0476a089186f03c44889fff7.gif')" });
+   }
+);
+
+$(".discord").hover(function () {
+      $(".stage").css({ "background": "url(https://i.gyazo.com/8c00820979ca19e74efb64fa9ae6a341.gif)" });
+   },
+   function () {
+      $(".stage").css({ "background": "https://i.gyazo.com/8c00820979ca19e74efb64fa9ae6a341.gif')" });
+   }
+);
+
+// Message to Developers
+console.log("This site will be under a lot of work.")
