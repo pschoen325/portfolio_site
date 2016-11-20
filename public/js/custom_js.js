373cc3c15033019933e6141b7474d71d79e54310
Paul Schoen <pschoen325@gmail.com>
2016-11-19 23:07:31 -0500

    added photos with jquery pluggin packery

--- public/js/custom_js.js
+++ public/js/custom_js.js
@@ -18,36 +18,47 @@ $(function(){
     });
 });
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+var __resetLayout = Packery.prototype._resetLayout;
+Packery.prototype._resetLayout = function() {
+  __resetLayout.call( this );
+  // reset packer
+  var parentSize = getSize( this.element.parentNode );
+  var colW = this.columnWidth + this.gutter;
+  this.fitWidth = Math.floor( ( parentSize.innerWidth + this.gutter ) / colW ) * colW;
+  this.packer.width = this.fitWidth;
+  this.packer.height = Number.POSITIVE_INFINITY;
+  this.packer.reset();
+};
+
+Packery.prototype._getContainerSize = function() {
+  // remove empty space from fit width
+  var emptyWidth = 0;
+  for ( var i=0, len = this.packer.spaces.length; i < len; i++ ) {
+    var space = this.packer.spaces[i];
+    if ( space.y === 0 && space.height === Number.POSITIVE_INFINITY ) {
+      emptyWidth += space.width;
     }
+  }
+
+  return {
+    width: this.fitWidth - this.gutter,
+    height: this.maxY - this.gutter
+  };
+};
+
+// always resize
+Packery.prototype.needsResizeLayout = function() {
+  return true;
+};
+
+$( function() {
+  var $container = $('.grid').packery({
+    itemSelector: '.grid-item',
+    columnWidth: 350,
+    gutter: 20
   });
 });
 
-$(".wynork").hover(function () {
-      $(".stage").css({ "background": "url('https://i.gyazo.com/ec05fe1c0476a089186f03c44889fff7.gif')" });
-   },
-   function () {
-      $(".stage").css({ "background": "https://i.gyazo.com/ec05fe1c0476a089186f03c44889fff7.gif')" });
-   }
-);
-
-$(".discord").hover(function () {
-      $(".stage").css({ "background": "url(https://i.gyazo.com/8c00820979ca19e74efb64fa9ae6a341.gif)" });
-   },
-   function () {
-      $(".stage").css({ "background": "https://i.gyazo.com/8c00820979ca19e74efb64fa9ae6a341.gif')" });
-   }
-);
 
 // Message to Developers
 console.log("This site will be under a lot of work.")
