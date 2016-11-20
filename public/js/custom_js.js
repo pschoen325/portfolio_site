97a819f2aba9e8882dfab0230b7fd04e86a3fd3a
Paul Schoen <pschoen325@gmail.com>
2016-11-19 23:12:42 -0500

    added anchor button 'art work'

--- public/js/custom_js.js
+++ public/js/custom_js.js
@@ -18,6 +18,21 @@ $(function(){
     });
 });
 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 var __resetLayout = Packery.prototype._resetLayout;
 Packery.prototype._resetLayout = function() {
   __resetLayout.call( this );
