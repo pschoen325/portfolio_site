07f0b37f527e2cf87d9cc7307bb9129f81e307fa
Paul Schoen <pschoen325@gmail.com>
2017-02-03 18:45:53 -0500

    Changed packery.js to run after the document loads

--- public/js/custom_js.js
+++ public/js/custom_js.js
@@ -16,69 +16,7 @@ $(document).ready(function () {
   });
   });
 
-  $("section").css("min-height", $(window).height() );
 
-  var __resetLayout = Packery.prototype._resetLayout;
-  Packery.prototype._resetLayout = function() {
-    __resetLayout.call( this );
-    // reset packer
-    var parentSize = getSize( this.element.parentNode );
-    var colW = this.columnWidth + this.gutter;
-    this.fitWidth = Math.floor( ( parentSize.innerWidth + this.gutter ) / colW ) * colW;
-    this.packer.width = this.fitWidth;
-    this.packer.height = Number.POSITIVE_INFINITY;
-    this.packer.reset();
-  };
-
-  Packery.prototype._getContainerSize = function() {
-    // remove empty space from fit width
-    var emptyWidth = 0;
-    for ( var i=0, len = this.packer.spaces.length; i < len; i++ ) {
-      var space = this.packer.spaces[i];
-      if ( space.y === 0 && space.height === Number.POSITIVE_INFINITY ) {
-        emptyWidth += space.width;
-      }
-    }
-
-    return {
-      width: this.fitWidth - this.gutter,
-      height: this.maxY - this.gutter
-    };
-  };
-
-  // always resize
-  Packery.prototype.needsResizeLayout = function() {
-    return true;
-  };
-
-  // Start baffle on any element(s).
-  let b = baffle('.type').start();
-  // Take care of some other stuff.
-    $(function(){
-        $(".type1").typed({
-            strings: ["Full Stack Developer"],
-            typeSpeed: 50,
-            showCursor: false,
-            startDelay: 1400
-        }
-      );
-    });
-    // Swap in new text and reveal over 1500ms.
-    setTimeout(1000);
-    b.reveal(1500);
-  $( function() {
-    var $container = $('.grid').packery({
-      itemSelector: '.grid-item',
-      columnWidth: 350,
-      gutter: 20
-    });
-  });
-  $grid.packery( 'bindDraggabillyEvents', draggie );
-  $grid.packery( 'bindUIDraggableEvents', $items );
-  // layout Packery after each image loads
-  $grid.imagesLoaded().progress( function() {
-    $grid.packery();
-  });
 
   // Message to Developers
   console.log("This site will be under a lot of work.");
@@ -134,6 +72,69 @@ $(document).ready(function () {
     };
   }
 
+});
+$("section").css("min-height", $(window).height() );
+
+var __resetLayout = Packery.prototype._resetLayout;
+Packery.prototype._resetLayout = function() {
+  __resetLayout.call( this );
+  // reset packer
+  var parentSize = getSize( this.element.parentNode );
+  var colW = this.columnWidth + this.gutter;
+  this.fitWidth = Math.floor( ( parentSize.innerWidth + this.gutter ) / colW ) * colW;
+  this.packer.width = this.fitWidth;
+  this.packer.height = Number.POSITIVE_INFINITY;
+  this.packer.reset();
+};
+
+Packery.prototype._getContainerSize = function() {
+  // remove empty space from fit width
+  var emptyWidth = 0;
+  for ( var i=0, len = this.packer.spaces.length; i < len; i++ ) {
+    var space = this.packer.spaces[i];
+    if ( space.y === 0 && space.height === Number.POSITIVE_INFINITY ) {
+      emptyWidth += space.width;
+    }
+  }
+
+  return {
+    width: this.fitWidth - this.gutter,
+    height: this.maxY - this.gutter
+  };
+};
+
+// always resize
+Packery.prototype.needsResizeLayout = function() {
+  return true;
+};
+
+// Start baffle on any element(s).
+let b = baffle('.type').start();
+// Take care of some other stuff.
+  $(function(){
+      $(".type1").typed({
+          strings: ["Full Stack Developer"],
+          typeSpeed: 50,
+          showCursor: false,
+          startDelay: 1400
+      }
+    );
+  });
+  // Swap in new text and reveal over 1500ms.
+  setTimeout(1000);
+  b.reveal(1500);
+$( function() {
+  var $container = $('.grid').packery({
+    itemSelector: '.grid-item',
+    columnWidth: 350,
+    gutter: 20
+  });
+});
+$grid.packery( 'bindDraggabillyEvents', draggie );
+$grid.packery( 'bindUIDraggableEvents', $items );
+// layout Packery after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.packery();
 });
 // GlitchText
 /*!
